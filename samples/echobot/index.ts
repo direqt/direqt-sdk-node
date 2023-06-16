84f9e98c40c01d352ea863b79a215b95fe511aad
Lauren Morgan <lauren.lolo.morgan@gmail.com>
2023-06-16 14:37:52 -0700

    updated the .json file to run simplier

--- samples/echobot/index.ts
+++ samples/echobot/index.ts
@@ -4,21 +4,33 @@
  * This sample demonstrates how to connect your chatbot to Direqt to send and
  * receive messages.
  */
-import express from 'express';
-//import { DireqtApi } from 'direqt';
-import { DireqtApi } from '../../src';
+
+
+import express , { Request, Response } from 'express' //! we need to have this! 
+import { DireqtApi } from 'direqt';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+//! Don't forget to add your access token and signing secret to your .env file
+
+
+const accessToken: string = process.env.DIREQT_ACCESS_TOKEN as string;
+const signingSecret: string = process.env.DIREQT_SIGNING_SECRET as string;
+if (!accessToken || !signingSecret) {
+    throw new Error('Missing DIREQT_ACCESS_TOKEN or DIREQT_SIGNING_SECRET');
+}
 
 const direqt = new DireqtApi({
-    accessToken: '<YOUR_ACCESS_TOKEN>',
-    signingSecret: '<YOUR_SIGNING_SECRET>',
+    accessToken,
+    signingSecret,
 });
 
 const app = express();
 
-const rawBodyExtractor = (req, res, buf) => {
-    req.rawBody = buf.toString();
+const rawBodyExtractor = (req: Request, res: Response, buf: Buffer) => {
+    (<any> req).rawBody = buf.toString();
 };
-
 app.use(
     '/webhook',
     express.json({ verify: rawBodyExtractor }),
